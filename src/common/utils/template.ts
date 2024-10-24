ee49baf3c6ec73531cc587db95d6e0bcaf33428a
kyanh0708 <nguyenkyanh.tanmai@gmail.com>
2024-10-24 23:10:43 +0700

    update template

--- src/common/utils/template.ts
+++ src/common/utils/template.ts
@@ -4,8 +4,8 @@ export const Template = {
    sendEmailNotiOtp: (otp: string) => {
       const emailHeader = "Change password account Antinotion";
       const emailBody = `
-                <h2>Antinotion</h2>
-                <p>Your otp code : ${otp}</p>
+                <h2>Antinotion Tab Manager</h2>
+                <p>Your OTP code is: ${otp}</p>
                 <p>Please back to the extension to confirm the code, the code expires in 5 minutes</p>
             `;
       return {
